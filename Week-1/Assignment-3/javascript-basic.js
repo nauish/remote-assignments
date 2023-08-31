052da817213f9594bde853f3957be4ac6a70e2c5
Rick <101254647+nauish@users.noreply.github.com>
2023-08-31 15:03:29 +0800

    Comment out added printed messages

--- Week-1/Assignment-3/javascript-basic.js
+++ Week-1/Assignment-3/javascript-basic.js
@@ -56,12 +56,12 @@ function toNumberAscii(input) {
   return numberArr;
 }
 
-console.log(toNumberAscii(["a", "b", "c", "a", "c", "a", "c"]));
-console.log(toNumberAscii(["e", "d", "c", "d", "e"]));
-
 let input1 = ["a", "b", "c", "a", "c", "a", "c"];
 console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
 console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
 let input2 = ["e", "d", "c", "d", "e"];
 console.log(countAandB(input2)); // should print 0
 console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
+
+// console.log(toNumberAscii(input1));
+// console.log(toNumberAscii(input2));
