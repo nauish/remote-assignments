4dea0b8e5fcdb52dc9178ff203e040ea56ffcbcd
Rick <101254647+nauish@users.noreply.github.com>
2023-09-04 12:03:39 +0800

    Implement request 2 click to show more content boxes

--- Week-2/Assignment-4/javascript.js
+++ Week-2/Assignment-4/javascript.js
@@ -1,7 +1,15 @@
-const goodDayMsg = "Have a Good Time!";
-
 const welcomeContainer = document.querySelector(".main-upper");
+const goodDayMsg = "Have a Good Time!";
 
 welcomeContainer.addEventListener("click", () => {
   welcomeContainer.innerHTML = `<h1>${goodDayMsg}</h1>`;
+});
+const callToActionBtn = document.querySelector(".call-to-action");
+const hiddenContainer = document.querySelector(".hidden-container");
+callToActionBtn.addEventListener("click", () => {
+  if (hiddenContainer.style.display === "flex") {
+    hiddenContainer.style.display = "none";
+  } else {
+    hiddenContainer.style.display = "flex";
+  }
 });
