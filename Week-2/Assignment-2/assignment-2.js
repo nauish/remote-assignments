e03cb8a3df60e2c47815bdc88a089ba469d8547a
Rick <101254647+nauish@users.noreply.github.com>
2023-09-04 10:51:28 +0800

    Implement the function

---
+++ Week-2/Assignment-2/assignment-2.js
@@ -0,0 +1,28 @@
+function avg(data) {
+  // your code here
+  let dataSum = 0;
+  for (let i = 0; i < data.size; i++) {
+    dataSum += data.products[i].price;
+  }
+  return dataSum / data.size;
+}
+
+console.log(
+  avg({
+    size: 3,
+    products: [
+      {
+        name: "Product 1",
+        price: 100,
+      },
+      {
+        name: "Product 2",
+        price: 700,
+      },
+      {
+        name: "Product 3",
+        price: 250,
+      },
+    ],
+  })
+); // should print the average price of all products
