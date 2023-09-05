3d0cd548dfbe5571761b660b015c1d397b51c893
Rick <101254647+nauish@users.noreply.github.com>
2023-09-05 19:15:56 +0800

    Fix comment typos

--- Week-2/Assignment-5/assignment-5.js
+++ Week-2/Assignment-5/assignment-5.js
@@ -5,16 +5,16 @@ function binarySearchPosition(numbers, target) {
   let right = numbers.length - 1;
 
   while (left <= right) {
-    // Calculate current middle value, and convert it to an interger
+    // Calculate current middle value, and convert it to an integer
     let currentMiddle = Math.floor((left + right) / 2);
-    // If the target is equal to the curret middle, return position
+    // If the target is equal to the current middle, return position
     if (target === numbers[currentMiddle]) {
       return currentMiddle;
-      // Else if the target is smaller than the curret middle,
+      // Else if the target is smaller than the current middle,
     } else if (target < numbers[currentMiddle]) {
-      // the target might be between position 0 - curret middle -1
+      // the target might be between position 0 - current middle -1
       right = currentMiddle - 1;
-      // Else if the target is larger than the curret middle,
+      // Else if the target is larger than the current middle,
     } else if (target > numbers[currentMiddle]) {
       // it might be between current middle position + 1 and the rightmost number
       left = currentMiddle + 1;
