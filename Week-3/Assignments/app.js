d6c6079e51cf5c2eb400a64f6d20b9de785bf78d
Rick <101254647+nauish@users.noreply.github.com>
2023-09-11 16:06:45 +0800

    Add /data route to serve trangular numbers based on request query

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Assignment 1
 app.get("/", (request, response) => {
   response.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`The server is listening on port ${port}`);
+  console.log(`The app is running on port ${port}`);
+});
+
+// Assignment 2
+app.get("/data", (request, response) => {
+  const n = +request.query.number;
+  if (isFinite(n) && n > 0) {
+    response.send(`${((1 + n) * n) / 2}`);
+  } else {
+    response.send("Wrong parameter!");
+  }
 });
