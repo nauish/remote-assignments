bb63b91500028c5c9fbd118475bce33965ce9592
Rick <101254647+nauish@users.noreply.github.com>
2023-09-12 14:18:39 +0800

    Remove redundant comments

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -19,7 +19,7 @@ app.use("/data", dataRoutes); // Assignment 2
 app.use("/myName", myNameRoutes); // Assignment 4
 app.use("/trackName", trackNameRoutes); // Assignment 4
 
-app.all("*", (req, res) => res.status(404).send("404 Page Not Found")); // 404 handler for undefined routes
+app.all("*", (req, res) => res.status(404).send("404 Page Not Found"));
 app.use(errorHandler); // 500 error handler for generic errors
 
 app.listen(port, () => console.log(`The app is running on port ${port}`));
