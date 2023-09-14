2256573753260b9eeb1ea63256b6245e1c5c0f77
Rick <101254647+nauish@users.noreply.github.com>
2023-09-14 18:57:36 +0800

    Update route logic when no parameter is passed in, add comment for calculation

--- Week-3/Assignments/routes/data.js
+++ Week-3/Assignments/routes/data.js
@@ -4,9 +4,13 @@ const router = express.Router();
 // Assignment 2: Build Backend API for Front-End
 router.get("/", (req, res, next) => {
   try {
-    const n = +req.query.number;
-    if (Number.isInteger(n) && n > 0) {
-      res.send(`${((1 + n) * n) / 2}`);
+    const { number } = req.query;
+    if (!number) {
+      res.send("Lack of parameter");
+    } else if (Number.isInteger(+number) && +number > 0) {
+      /** Since the algorithm for calculating triangle numbers (1 + n )* n / 2 calculates the result directly.
+          it should have the time complexity of O(1), which would be suitable for calculating large numbers */
+      res.send(`${((1 + +number) * +number) / 2}`);
     } else {
       res.status(400).send("Wrong parameter!");
     }
