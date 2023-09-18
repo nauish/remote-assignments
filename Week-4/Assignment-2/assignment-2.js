31e4b8c859cc723f7e49317ad789aca1353edd8f
Rick <101254647+nauish@users.noreply.github.com>
2023-09-18 11:48:53 +0800

    Implement ajax function

--- Week-4/Assignment-2/assignment-2.js
+++ Week-4/Assignment-2/assignment-2.js
@@ -1,5 +1,13 @@
 function ajax(src, callback) {
   // your code here
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open("GET", src);
+  xhr.send();
 }
 function render(data) {
   // your code here
