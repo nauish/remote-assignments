30b7f660961b5d443fd474ad93cf3cf455bc8971
Rick <101254647+nauish@users.noreply.github.com>
2023-09-19 22:11:07 +0800

    Delete deprecated route

--- Week-4/Assignment-3/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const helloWorld = require("../controllers/helloWorld");
-const router = express.Router();
-
-// Assignment 1: Your First Web Server
-router.get("/", helloWorld);
-
-module.exports = router;
