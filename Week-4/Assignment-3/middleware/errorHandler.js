aa95ef2795647bbb4e4c4d73f15de6c69b477be1
Rick <101254647+nauish@users.noreply.github.com>
2023-09-21 14:07:53 +0800

    Refactor to serve json

--- Week-4/Assignment-3/middleware/errorHandler.js
+++ Week-4/Assignment-3/middleware/errorHandler.js
@@ -1,4 +1,4 @@
 export default (err, req, res, next) => {
   console.log(err.stack);
-  res.status(500).send("Internal Server Error");
+  res.status(500).json({ success: false, message: "Internal Server Error!" });
 };
