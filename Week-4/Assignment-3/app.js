c896ae52cd29b7a1f580c3a076f32a199c7aa370
Rick <101254647+nauish@users.noreply.github.com>
2023-09-21 13:48:30 +0800

    Add CORS

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
 import authRoutes from "./routes/auth.js";
-import session from "express-session";
+import memberRoutes from "./routes/member.js";
+import session from "express-session"; // Store user session
 import { config } from "dotenv";
+import cors from "cors";
 
 config();
 
@@ -19,21 +21,19 @@ app.use(
   })
 );
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
+
 app.use("/", authRoutes);
+app.use("/member", memberRoutes);
 
-app.get("/member", (req, res) => {
-  if (req.session.status) {
-    res.render("member.ejs", { email: req.session.email });
-  } else {
-    res.redirect("/");
-  }
-});
-
-app.all("*", (req, res) => res.status(404).send("404 Page Not Found"));
+app.all("*", (req, res) =>
+  res.status(404).json({ error: "404 Page Not Found" })
+);
 app.use(errorHandler); // 500 error handler for generic errors
 
 app.listen(port, () => console.log(`The app is running on port ${port}`));
