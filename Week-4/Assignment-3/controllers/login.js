02d8ce8ade5f3f6d0f825d92da5ecea3379f9a54
Rick <101254647+nauish@users.noreply.github.com>
2023-09-22 15:35:29 +0800

    Refactor code from nested conditions to early return

--- Week-4/Assignment-3/controllers/login.js
+++ Week-4/Assignment-3/controllers/login.js
@@ -5,20 +5,23 @@ const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const [user] = await selectUserByEmail(email);
-    if (user) {
-      const validPassword = await compare(password, user.password);
-      if (validPassword) {
-        req.session.status = "login success";
-        req.session.email = `${email}`;
-        res
-          .status(200)
-          .json({ success: true, message: "Valid Email and Password" });
-      } else {
-        res.status(401).json({ success: false, message: "Wrong Password!" });
-      }
-    } else {
-      res.status(404).json({ success: false, message: "Email not found!" });
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: "Email not found!" });
+
+    const validPassword = await compare(password, user.password);
+    if (!validPassword) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Wrong Password!" });
     }
+
+    req.session.status = "login success";
+    req.session.email = `${email}`;
+    res
+      .status(200)
+      .json({ success: true, message: "Valid Email and Password" });
   } catch (error) {
     console.log(error);
     next(error);
